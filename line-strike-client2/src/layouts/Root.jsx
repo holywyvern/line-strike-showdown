adeafd18b8e4f37031a408e743f41f9936303012
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2024-07-25 07:22:22 -0300

    Bring back old background

--- line-strike-client2/src/layouts/Root.jsx
+++ line-strike-client2/src/layouts/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet, useLoaderData } from "react-router-dom";
 import { DatabaseContext } from "../contexts/DatabaseContext";
+import { LineStrikeBackground } from "../design/LineStrikeBackground";
 
 export function Root() {
   const { database } = useLoaderData();
   return (
     <DatabaseContext.Provider value={database}>
-      <Outlet />
+      <LineStrikeBackground />
+      <div style={{ position: "absolute", inset: 0, overflow: "hidden" }}>
+        <Outlet />
+      </div>
     </DatabaseContext.Provider>
   );
 }
