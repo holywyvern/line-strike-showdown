6791e63ba8a6a172482d40e9989f1ae1e9ea9d29
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2024-07-25 08:52:41 -0300

    Add PWA manifest

--- line-strike-client2/vite.config.js
+++ line-strike-client2/vite.config.js
@@ -1,9 +1,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: ["icon.webp"],
+  manifest: {
+    name: "Line Strike Showdown",
+    short_name: "line-strike-showdown",
+    description: "Play Line strike in your browser",
+    icons: [
+      {
+        src: "/app/android-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/app/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/app/apple-180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/app/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#14669d",
+    background_color: "#000000",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "any",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   server: {
     proxy: {
       "/api": {
