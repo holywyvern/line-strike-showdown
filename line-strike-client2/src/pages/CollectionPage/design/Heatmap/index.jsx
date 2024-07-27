f584f126c253c340112a72273a55858db5128882
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2024-07-27 05:05:45 -0300

    add nicer indicators

--- line-strike-client2/src/pages/CollectionPage/design/Heatmap/index.jsx
+++ line-strike-client2/src/pages/CollectionPage/design/Heatmap/index.jsx
@@ -9,25 +9,41 @@ export function Heatmap({ area, enemy, flip }) {
   const max = Math.max(...area);
   const diff = max === min ? 1 : max - min;
   return (
-    <div className={className}>
-      {area.map((i, index) => {
-        let intensity = (i - min) / diff;
-        if (flip) {
-          intensity = 1 - intensity;
-        }
-        return (
-          <div
-            key={index}
-            className={cx(styles.item, { [styles.active]: i })}
-            style={{ "--intensity": intensity }}
-          >
-            <div className={styles.wrapper}>
-              {i ? Math.floor(i * 100) / 100 : 0}
+    <>
+      {enemy ? null : (
+        <div className={cx(styles.fake, styles.enemy)}>
+          <div className={styles.ref} />
+          <div className={styles.ref} />
+          <div className={styles.ref} />
+        </div>
+      )}
+      <div className={className}>
+        {area.map((i, index) => {
+          let intensity = (i - min) / diff;
+          if (flip) {
+            intensity = 1 - intensity;
+          }
+          return (
+            <div
+              key={index}
+              className={cx(styles.item, { [styles.active]: i })}
+              style={{ "--intensity": intensity }}
+            >
+              <div className={styles.wrapper}>
+                {i ? Math.floor(i * 100) / 100 : 0}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      {!enemy ? null : (
+        <div className={cx(styles.fake)}>
+          <div className={styles.ref} />
+          <div className={styles.ref} />
+          <div className={styles.ref} />
+        </div>
+      )}
+    </>
   );
 }
 
