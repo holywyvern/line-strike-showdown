c141d2dd1f78ddccc8a016cf553639cd255d28ed
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2024-07-28 18:53:21 -0300

    fix multiple join room

--- line-strike-client2/src/services/colyseus.js
+++ line-strike-client2/src/services/colyseus.js
@@ -3,6 +3,8 @@ import { Client } from "colyseus.js";
 
 export const client = new Client("/api");
 
+let lastName, lastToken, lastRoom;
+
 export const ColyseusService = {
   get client() {
     return client;
@@ -11,7 +13,13 @@ export const ColyseusService = {
     return client.joinOrCreate("lobby");
   },
   async joinLobby(name, token = null) {
+    if (lastName === name && lastToken === token) {
+      return lastRoom;
+    }
     const room = await client.join("showdown_lobby", { name, token });
+    lastRoom = room;
+    lastName = name;
+    lastToken = token;
     return new Promise((resolve) => {
       room.onStateChange.once(() => resolve(room));
     });
